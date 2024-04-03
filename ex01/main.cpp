e462443cd9706baf9782d877dd056287f10f0282
chustei <chustei@student.42berlin.de>
2024-04-03 12:20:58 +0200

    ex01 -- done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,49 +3,47 @@
 #include "./inc/Dog.hpp"
 
 
-int main() {
-/*     const Animal* j = new Cat();
-
-
-    // Call the copy constructor explicitly
-    const Cat* CatCopy = new Cat(*dynamic_cast<const Cat*>(j));
-
-    std::cout << CatCopy->getIdeaFromBrain(5) << std::endl;
-    std::cout << j->getIdeaFromBrain(5) << std::endl;
-
-
-    delete j;
-    delete CatCopy; // Remember to delete the dynamically allocated copy */
-	Dog cat1;
-	Dog cat2;
-
-	cat2 = cat1;
-
-	std::cout << cat1.getIdeaFromBrain(5) << std::endl;
-	std::cout << cat2.getIdeaFromBrain(5) << std::endl;
-
-
-	return 0;
-}
-
-int test()
+int main()
 {
-	//const Animal* animal = new Animal();
-	const Animal* j = new Dog();
-	//const Animal* i = new Cat();
-	//const Dog* dog__ = new Dog();
+	const Animal* animals[8];
+	const Animal* copyAnimals[8];
 
-	const Animal* copy(j);
+	for (int i = 0; i < 8; i++) {
+		std::cout << i <<")-------------------------------" << std::endl;
+		if (i < 4)
+			animals[i] = new Dog;
+		else
+			animals[i] = new Cat;
+		std::cout << i << ")-------------------------------\n" << std::endl;
+	}
 
-	std::cout << copy->getIdeaFromBrain(5)<< std::endl;
-	//std::cout << j->getIdeaFromBrain(5)<< std::endl;
-	//std::cout << animal->getIdeaFromBrain(5)<< std::endl;
-	//std::cout << i->getIdeaFromBrain(5)<< std::endl;
+	for (int i = 0; i < 8; i++) {
+		std::cout << i <<")+++++++++++++++++++++++++++++++" << std::endl;
+		animals[i]->makeSound();
+		std::cout << i << ")+++++++++++++++++++++++++++++++\n" << std::endl;
+	}
 
-/* 	delete animal;
-	delete j;
-	delete i; */
-	delete j;
+	for (int i = 0; i < 8; i++) {
+		std::cout << i <<")+++++++++++++++++++++++++++++++" << std::endl;
+		if (i < 4)
+			copyAnimals[i] = new Dog(*dynamic_cast<const Dog*>(animals[i]));
+		else
+			copyAnimals[i] = new Cat(*dynamic_cast<const Cat*>(animals[i]));
+		std::cout << "Animal idea: " << animals[i]->getIdeaFromBrain(i) << std::endl;
+		std::cout << "CopyAnimal idea: " << copyAnimals[i]->getIdeaFromBrain(i) << std::endl;
+		std::cout << i << ")+++++++++++++++++++++++++++++++\n" << std::endl;
+	}
 
+	for (int i = 0; i < 8; i++) {
+		std::cout << i <<")-------------------------------" << std::endl;
+		delete animals[i];
+		std::cout << i << ")-------------------------------\n" << std::endl;
+	}
+
+	for (int i = 0; i < 8; i++) {
+		std::cout << i <<")-------------------------------" << std::endl;
+		delete copyAnimals[i];
+		std::cout << i << ")-------------------------------\n" << std::endl;
+	}
 	return 0;
 }
